94e4244a46602c9f1c276f39e0d784c334fae831
suarezmanuel <manuelsuarez244@gmail.com>
2024-03-13 14:59:33 +0200

    fixed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const customEnv = require('custom-env');
 
 customEnv.env(process.env.NODE_ENV, './config');
 
-const users = require('./routes/user')
+const users = require('./routes/users')
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -19,5 +19,10 @@ server.use(express.static('public'))
 server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.json())
-server.use('/user', users)
+
+// this is the magic
+server.use('/api/users', users)
+
+// server.use('/api/users', users)
+
 server.listen(process.env.SERVER_PORT)
