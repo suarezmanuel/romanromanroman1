14bed9c19f204ed1474a76a92dd70b3b8da7aeca
mike shlapakov <mike.shlapakov@gmail.com>
2024-03-15 16:29:27 +0200

    finished Tokens

--- controller/tokens.js
+++ controller/tokens.js
@@ -1,13 +1,13 @@
 const tokensService = require('../services/tokens');
 
-const isLoggedIn = async (req, res) => {
-    console.log(req.headers.authorization, req.params.id)
+const isLoggedIn = async (req, res, next) => {
+    // console.log(req.headers.authorization, req.params.id)
     let status = await tokensService.isLoggedIn(req.headers.authorization, req.params.id);
     console.log(status)    
     if (status == 401) return res.status(401).json({ error: "Invalid Token" });
     if (status == 403) return res.status(403).json({ error: "Token required" });
     if (status == 404) return res.status(404).json({ error: "Couldn't verify token" });
-    if (status == 200) return res.status(200).json({ data: "token verifyed successfully" });
+    if (status == 200) return next();
 }
 
 const createToken = async (req, res) => {
